5b62146e3388a848ca00ad3da699a0846dcef959
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-03-02 08:03:49 -1100

    Added the the sample raw  users for testing the application

--- src/main/java/com/jazzybruno/example/v1/config/JwtAuthFilter.java
+++ src/main/java/com/jazzybruno/example/v1/config/JwtAuthFilter.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -17,7 +18,7 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Component
 @RequiredArgsConstructor
-public class JwtAuthFilter {
+public class JwtAuthFilter extends OncePerRequestFilter {
     private final UserDao userDao;
     private final JwtUtils jwtUtils;
 
---
+++ src/main/java/com/jazzybruno/example/v1/dao/UserDao.java
@@ -0,0 +1,34 @@
+package com.jazzybruno.example.v1.dao;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class UserDao {
+    private final static List<UserDetails> APPLICATION_USERS = Arrays.asList(
+            new User(
+                    "jazzybruno45@gmail.com",
+                    "password",
+                    Collections.singletonList(new SimpleGrantedAuthority("ROLE_ADMIN"))
+            ),
+
+            new User(
+                    "axellemugisha@gmail.com",
+                    "password",
+                    Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
+            )
+    );
+
+    public UserDetails findUserByEmail(String email){
+        return   APPLICATION_USERS
+                .stream()
+                .filter(u -> u.getUsername().equals(email))
+                .findFirst()
+                .orElseThrow(() -> new UsernameNotFoundException("The user was not found"));
+    }
+}
