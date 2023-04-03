190e5ace8051a6695e2ca4ef89c54afd138a022f
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-04-04 00:23:16 +0200

    Added listerners to some exceptions  thrown in the jwt authentication filter

--- src/main/java/com/jazzybruno/example/v1/security/jwt/JwtAuthFilter.java
+++ src/main/java/com/jazzybruno/example/v1/security/jwt/JwtAuthFilter.java
@@ -9,6 +9,7 @@ import com.jazzybruno.example.v1.security.user.UserSecurityDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -29,21 +30,49 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-            final String authHeader = request.getHeader("Authorization");
-             JwtUserInfo jwtUserInfo = null;
-             String jwtToken = null;
+        final String authHeader = request.getHeader("Authorization");
+        JwtUserInfo jwtUserInfo = null;
+        String jwtToken = null;
 
-            if(authHeader == null || !authHeader.startsWith("Bearer")){
-                filterChain.doFilter(request , response);
-                return;
-            }
+        if (authHeader == null || !authHeader.startsWith("Bearer")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
-            jwtToken = authHeader.substring(7);
+        jwtToken = authHeader.substring(7);
+
+        try {
+            jwtUserInfo = jwtUtils.decodeToken(jwtToken);
+        } catch (JWTVerificationException e) {
+            TokenException exception = new TokenException(e.getMessage());
+
+            // the repsonse type and status
+            response.setStatus(exception.getResponseEntity().getStatusCodeValue());
+            response.setContentType("application/json");
+
+            // set a new response object as a json one
+            ObjectMapper objectMapper = new ObjectMapper();
+            response.getWriter().write(objectMapper.writeValueAsString(exception.getResponseEntity().getBody()));
+
+            // exit the filter chain
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        if (jwtUserInfo.getEmail() != null && SecurityContextHolder.getContext().getAuthentication() == null) {
 
             try {
-                jwtUserInfo = jwtUtils.decodeToken(jwtToken);
-            }catch (JWTVerificationException e){
-                TokenException exception =new TokenException(e.getMessage());
+                UserSecurityDetails userSecurityDetails = (UserSecurityDetails) userSecurityDetailsService.loadUserByUsername(jwtUserInfo.getEmail());
+                if (jwtUtils.isTokenValid(jwtToken, userSecurityDetails)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            userSecurityDetails, jwtToken, userSecurityDetails.getGrantedAuthorities()
+                    );
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+                filterChain.doFilter(request, response);
+            } catch (UsernameNotFoundException e) {
+                TokenException exception = new TokenException(e.getMessage());
 
                 // the repsonse type and status
                 response.setStatus(exception.getResponseEntity().getStatusCodeValue());
@@ -54,23 +83,9 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                 response.getWriter().write(objectMapper.writeValueAsString(exception.getResponseEntity().getBody()));
 
                 // exit the filter chain
-                filterChain.doFilter(request , response);
-                return;
+                filterChain.doFilter(request, response);
             }
 
-            if(jwtUserInfo.getEmail() != null && SecurityContextHolder.getContext().getAuthentication() == null){
-                UserSecurityDetails userSecurityDetails = (UserSecurityDetails) userSecurityDetailsService.loadUserByUsername(jwtUserInfo.getEmail());
-
-                if(jwtUtils.isTokenValid(jwtToken , userSecurityDetails)){
-                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                            userSecurityDetails , jwtToken , userSecurityDetails.getGrantedAuthorities()
-                    );
-                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(authToken);
-                }else{
-                }
-            }
-            filterChain.doFilter(request , response);
-
+        }
     }
 }
